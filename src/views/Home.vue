80d3852948309562dc0a21074973b1865c5e25db
xiaolifeipiao <1921513506@qq.com>
2021-06-15 15:54:04 +0800

    [view] 测试提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <div>主页</div>
+  <img src="" alt="logo">
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+<script>
+export default {
+  name: "Home"
+}
 </script>
+
+<style scoped>
+
+</style>
