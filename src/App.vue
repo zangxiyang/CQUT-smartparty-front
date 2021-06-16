fd126f58945359760699436fb714913d9b1caadf
xiaolifeipiao <1921513506@qq.com>
2021-06-16 18:27:38 +0800

    [view] nav导航条页面的实现

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,20 @@
 <template>
   <Header/>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Nav/>
   <Footer/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Nav from "@/components/Nav";
 
 export default {
   name: 'App',
   components: {
     Header,
     Footer,
-    HelloWorld
+    Nav,
   }
 }
 </script>
