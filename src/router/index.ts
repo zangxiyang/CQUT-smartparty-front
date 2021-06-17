4dddf25187903bb89539351844281a3154921c5a
Seale <1801157108@qq.com>
2021-06-17 21:44:58 +0800

    [fix] 修复router配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,49 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import {clearPending} from '@/utils/https'
+
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'AllPartyMember',
-    component: ()=> import('../views/AllPartyMember.vue')
-  },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+    {
+        path: '/',
+        name: 'AllPartyMember',
+        component: () => import('../views/AllPartyMember.vue'),
+        meta: {
+            keepAlive: true,
+        }
+    }, {
+        path: '/member/:id',
+        name: 'MemberDetail',
+        component: () => import('../views/MemberDetail.vue'),
+        meta: {
+            keepAlive: true
+        }
+    }
+    // {
+    //   path: '/about',
+    //   name: 'About',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 router.beforeEach((to, from, next) => {
-  //在跳转路由之前，先清除所有的请求
-  clearPending()
-  // ...
-  next()
+    //在跳转路由之前，先清除所有的请求
+    clearPending()
+    // ...
+    next()
+})
+
+router.afterEach(() => {
+    // 保证切换路由的时候页面在最顶部
+    window.scrollTo({
+        top: 0
+    })
 })
 
 export default router
