288b2bd343f5c35ebc2c8ed06e14aba51bfe7541
Seale <1801157108@qq.com>
2021-06-25 19:45:15 +0800

    [fix] 修复年度发展党员逻辑问题

--- src/views/Expand/ExpandMember.vue
+++ src/views/Expand/ExpandMember.vue
@@ -18,7 +18,7 @@
                      :page-size="pagination.pageSize"
                      :current-page="pagination.currPage"
                      :page-count="pagination.totalPage"
-                     @current-change="selectContainerClick"
+                     @current-change="currentChange"
                      class="mt-20"/>
     </div>
   </select-container-layout>
@@ -31,7 +31,7 @@
  * 作者: Seale
  * 时间: 2021/6/20
  * 版本: V1
-*/
+ */
 import {defineComponent, ref} from 'vue';
 import SelectContainerLayout from "@/layout/SelectContainerLayout.vue";
 import MemberList from "@/components/MemberList.vue";
@@ -43,7 +43,7 @@ import {IPagination} from "@/models/IPagination";
 export default defineComponent({
   name: "ExpandMember",
   components: {MemberList, SelectContainerLayout},
-  setup(){
+  setup() {
     const active = ref(1);
     const yearActive = ref('');
     const yearNums = ref();
@@ -61,18 +61,26 @@ export default defineComponent({
     // 成员列表
     const memberList = ref([] as IMemberList);
     // 加载党支部的数据
-    const loadOrgInfo = async (orgId: number): Promise=>{
+    const loadOrgInfo = async (orgId: number): Promise => {
       options.value = []; //清空数据
-      await BranchWorkService.getPartyGrowInfo(orgId).then(res=>{
+      await BranchWorkService.getPartyGrowInfo(orgId).then(res => {
         options.value = res.data;
-        yearActive.value = options.value[0].year; // 默认为第一项选中
-        yearNums.value = options.value[0].num;    // 默认为第一项的人数
+        try {
+          yearActive.value = options?.value[0].year; // 默认为第一项选中
+          yearNums.value = options?.value[0].num;    // 默认为第一项的人数
+        }catch (e){
+          // 清空数据
+          memberList.value = []
+          options.value = []
+          yearActive.value = ''
+          yearNums.value = '空'
+        }
       })
     }
     // 加载列表数据
-    const loadData = (year,orgId = 1,page= 1): void=>{
+    const loadData = (year, orgId = 1, page = 1): void => {
       memberList.value = []
-      BranchWorkService.getPartyGrowList(year,orgId,page,12).then(res=>{
+      BranchWorkService.getPartyGrowList(year, orgId, page, 12).then(res => {
         const {list, ...params} = res.data
         memberList.value = list     //   填充数据
         pagination.value = params;  //   初始化pagination对象
@@ -80,40 +88,48 @@ export default defineComponent({
     }
 
     // 初始化方法
-    const initLoad = () =>{
+    const initLoad = () => {
       // 初始化执行的请求流程 ：1. 首先加载orgInfo信息，之后加载data信息
-      loadOrgInfo(active.value).then(()=>{
+      loadOrgInfo(active.value).then(() => {
         loadData(yearActive.value)
       })
     }
-    // 分页被改变页数
-    const selectContainerClick = (page: number)=>{
-      loadData(yearActive.value,active.value,page);
+
+
+    // 选择支部被改变
+    const selectContainerClick = () => {
+      loadOrgInfo(active.value).then(() => {
+        loadData(yearActive.value, active.value)
+      })
     }
+
+    // 分页被改变页数
+    const currentChange = (page: number) => {
+      loadData(yearActive.value, active.value, page);
+    }
+
     // 选择框被改变
-    const selectChange = (val: string) =>{
+    const selectChange = (val: string) => {
       // 首先查找在options中对应年份的值
-      options.value.forEach((o: object)=>{
-        if (o.year == val){
+      options.value.forEach((o: object) => {
+        if (o.year == val) {
           yearNums.value = o.num;
         }
       });
       // 之后加载数据
-      loadData(val,active.value,1)
+      loadData(val, active.value, 1)
     }
 
 
-    const click = (id: string)=>{
+    const click = (id: string) => {
       router.push(`/branch/member/${id}`);
     }
-
-
 
 
     // ====================================================================================
     initLoad();
 
-    return{
+    return {
       active,
       yearActive,
       click,
@@ -122,18 +138,21 @@ export default defineComponent({
       yearNums,
       memberList,
       options,
-      pagination
+      pagination,
+      currentChange,
+      selectContainerClick
     }
   }
 })
 </script>
 
 <style lang="scss" scoped>
-.header{
+.header {
   padding-bottom: 15px;
   border-bottom: 1px solid #eee;
 }
-.content{
+
+.content {
   padding-top: 20px;
 }
 </style>
